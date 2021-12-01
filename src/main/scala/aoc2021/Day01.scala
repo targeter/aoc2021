bcb90ba764eb9e10428d1956ea86c03be1fb91e6
Sietse de Kaper <targeter@gmail.com>
2021-12-01 07:04:19 +0100

    Day 1

--- src/main/scala/aoc2021/Day01.scala
+++ src/main/scala/aoc2021/Day01.scala
@@ -3,9 +3,17 @@ package aoc2021
 import shared._
 
 case object Day01 extends AocTools(day = 1) {
-  def step1: Option[Int] = ???
 
-  def step2: Option[Int] = ???
+  def step1: Int = step1(inputInts)
+
+  def step1(measurements: Seq[Int]): Int = measurements
+    .foldLeft((0, Integer.MAX_VALUE)) {
+      case ((count, prev), curr) if curr > prev => count + 1 -> curr
+      case ((count, _), curr)                   => count -> curr
+    }
+    ._1
+
+  def step2: Int = step1(inputInts.sliding(3).map(_.sum).toSeq)
 
   def main(args: Array[String]): Unit = {
     println("Step 1: " + step1)
